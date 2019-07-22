1a2eeaba0380ec93f00c85fc020c8c795cbd699a
ml250271 <lukicmilica@hotmail.com>
2019-07-22 14:19:47 +0200

    add correction to test description

--- test.js
+++ test.js
@@ -3,202 +3,192 @@ const assert = require("assert");
 const { Builder, By, until } = require("selenium-webdriver");
 
 describe("Tests", function() {
-    let driver;
-    before(async function() {
-        driver = await new Builder().forBrowser("chrome").build();
-    });
+  let driver;
+  before(async function() {
+    driver = await new Builder().forBrowser("chrome").build();
+  });
 
-    it("Exam #1 - Validate sum of 5 integers", async function() {
-        await driver.get("https://itk-qa-exams.itekako.com/first");
+  it("Exam #1 - Validate sum of 5 integers", async function() {
+    await driver.get("https://itk-qa-exams.itekako.com/first");
 
-        const elements = await driver.findElements(By.className('"value"'));
-        const promises = elements.map(el => el.getText());
-        const numbers = await Promise.all(promises);
-        const sum = numbers.reduce((total, num) => (total += Number(num)), 0);
-        let result = await driver
-            .findElement(By.className('"result"'))
-            .getText();
-        result = Number(result);
+    const elements = await driver.findElements(By.className('"value"'));
+    const promises = elements.map(el => el.getText());
+    const numbers = await Promise.all(promises);
+    const sum = numbers.reduce((total, num) => (total += Number(num)), 0);
+    let result = await driver.findElement(By.className('"result"')).getText();
+    result = Number(result);
 
-        assert(result === sum, "Exam #1 ERROR!");
-    });
+    assert(result === sum, "Exam #1 ERROR!");
+  });
 
-    it("Exam #2 - Validate result of 5 integers and random operation(- or + or * or /)", async function() {
-        await driver.get("https://itk-qa-exams.itekako.com/second");
+  it("Exam #2 - Validate result of 5 integers and random operation(- or + or * or /)", async function() {
+    await driver.get("https://itk-qa-exams.itekako.com/second");
 
-        const spans = await driver.findElements(By.tagName("span"));
-        const promisesSpans = spans.map(el => el.getText());
-        const operation = await Promise.all(promisesSpans);
-        operation.unshift("0");
-        const elements = await driver.findElements(By.className('"value"'));
-        const promisesEl = elements.map(el => el.getText());
-        const numbers = await Promise.all(promisesEl);
-        let result = await driver
-            .findElement(By.className('"result"'))
-            .getText();
-        result = Number(result);
-        let total = Number(numbers[0]);
-        for (let i = 1; i < numbers.length; i++) {
-            if (operation[i] == "+") {
-                total += Number(numbers[i]);
-            } else if (operation[i] == "-") {
-                total -= Number(numbers[i]);
-            } else if (operation[i] == "*") {
-                total *= Number(numbers[i]);
-            } else if (operation[i] == "/") {
-                total /= Number(numbers[i]);
-            }
-        }
-        if (total % 1 !== 0) {
-            total = Math.round(total * 100) / 100;
-        }
-        assert(result === total, "Exam #2 ERROR!");
-    });
+    const spans = await driver.findElements(By.tagName("span"));
+    const promisesSpans = spans.map(el => el.getText());
+    const operation = await Promise.all(promisesSpans);
+    operation.unshift("0");
+    const elements = await driver.findElements(By.className('"value"'));
+    const promisesEl = elements.map(el => el.getText());
+    const numbers = await Promise.all(promisesEl);
+    let result = await driver.findElement(By.className('"result"')).getText();
+    result = Number(result);
+    let total = Number(numbers[0]);
+    for (let i = 1; i < numbers.length; i++) {
+      if (operation[i] == "+") {
+        total += Number(numbers[i]);
+      } else if (operation[i] == "-") {
+        total -= Number(numbers[i]);
+      } else if (operation[i] == "*") {
+        total *= Number(numbers[i]);
+      } else if (operation[i] == "/") {
+        total /= Number(numbers[i]);
+      }
+    }
+    if (total % 1 !== 0) {
+      total = Math.round(total * 100) / 100;
+    }
+    assert(result === total, "Exam #2 ERROR!");
+  });
 
-    it("Exam #3 - Validate result of variable number of operands and fixed operation (multiplication)", async function() {
-        await driver.get("https://itk-qa-exams.itekako.com/third");
+  it("Exam #3 - Validate result of variable number of operands and fixed operation (multiplication)", async function() {
+    await driver.get("https://itk-qa-exams.itekako.com/third");
 
-        const elements = await driver.findElements(By.className('"value"'));
-        const promises = elements.map(el => el.getText());
-        const numbers = await Promise.all(promises);
-        const sum = numbers.reduce((total, num) => (total *= Number(num)), 1);
-        let result = await driver
-            .findElement(By.className('"result"'))
-            .getText();
-        result = Number(result);
-        assert(result === sum, "Exam #3 ERROR!");
-    });
+    const elements = await driver.findElements(By.className('"value"'));
+    const promises = elements.map(el => el.getText());
+    const numbers = await Promise.all(promises);
+    const sum = numbers.reduce((total, num) => (total *= Number(num)), 1);
+    let result = await driver.findElement(By.className('"result"')).getText();
+    result = Number(result);
+    assert(result === sum, "Exam #3 ERROR!");
+  });
 
-    it("Exam #4 - Validate result of variable number of operands and alternates between multiplication and division operators", async function() {
-        await driver.get("https://itk-qa-exams.itekako.com/fourth");
+  it("Exam #4 - Validate result of variable number of operands and alternates between multiplication and division operators", async function() {
+    await driver.get("https://itk-qa-exams.itekako.com/fourth");
 
-        const spans = await driver.findElements(By.tagName("span"));
-        const promisesSpans = spans.map(el => el.getText());
-        const operation = await Promise.all(promisesSpans);
-        operation.unshift("0");
-        const elements = await driver.findElements(By.className('"value"'));
-        const promisesEl = elements.map(el => el.getText());
-        const numbers = await Promise.all(promisesEl);
-        let result = await driver
-            .findElement(By.className('"result"'))
-            .getText();
-        result = Number(result);
-        let total = Number(numbers[0]);
-        for (i = 1; i < numbers.length; i++) {
-            if (operation[i] == "*") {
-                total *= Number(numbers[i]);
-            } else if (operation[i] == "/") {
-                total /= Number(numbers[i]);
-            }
-        }
-        if (total % 1 !== 0) {
-            total = Math.round(total * 100) / 100;
-        }
-        assert(result === total, "Exam #4 ERROR!");
-    });
+    const spans = await driver.findElements(By.tagName("span"));
+    const promisesSpans = spans.map(el => el.getText());
+    const operation = await Promise.all(promisesSpans);
+    operation.unshift("0");
+    const elements = await driver.findElements(By.className('"value"'));
+    const promisesEl = elements.map(el => el.getText());
+    const numbers = await Promise.all(promisesEl);
+    let result = await driver.findElement(By.className('"result"')).getText();
+    result = Number(result);
+    let total = Number(numbers[0]);
+    for (i = 1; i < numbers.length; i++) {
+      if (operation[i] == "*") {
+        total *= Number(numbers[i]);
+      } else if (operation[i] == "/") {
+        total /= Number(numbers[i]);
+      }
+    }
+    if (total % 1 !== 0) {
+      total = Math.round(total * 100) / 100;
+    }
+    assert(result === total, "Exam #4 ERROR!");
+  });
 
-    it("Exam #5 Validate palindrome", async function() {
-        await driver.get("https://itk-qa-exams.itekako.com/fifth");
+  it("Exam #5 - Validate palindrome", async function() {
+    await driver.get("https://itk-qa-exams.itekako.com/fifth");
 
-        const wordEl = await driver.findElement(By.id("word"));
-        const word = await wordEl.getText();
-        const resultEl = await driver.findElement(By.id("result"));
-        const result = await resultEl.getText();
-        const wordArr = word.toLowerCase().split("");
-        const numForSlice = (wordArr.length / 2) | 0;
-        let myResult = "Yes";
-        for (let i = 0; i < numForSlice; i++) {
-            if (wordArr[i] !== wordArr[wordArr.length - 1 - i]) {
-                myResult = "No";
-            }
-        }
-        assert(result === myResult, "Exam #5 ERROR!");
-    });
+    const wordEl = await driver.findElement(By.id("word"));
+    const word = await wordEl.getText();
+    const resultEl = await driver.findElement(By.id("result"));
+    const result = await resultEl.getText();
+    const wordArr = word.toLowerCase().split("");
+    const numForSlice = (wordArr.length / 2) | 0;
+    let myResult = "Yes";
+    for (let i = 0; i < numForSlice; i++) {
+      if (wordArr[i] !== wordArr[wordArr.length - 1 - i]) {
+        myResult = "No";
+      }
+    }
+    assert(result === myResult, "Exam #5 ERROR!");
+  });
 
-    it("Exam #6 - Validate individual letter count", async function() {
-        await driver.get("https://itk-qa-exams.itekako.com/sixth");
+  it("Exam #6 - Validate individual letter count", async function() {
+    await driver.get("https://itk-qa-exams.itekako.com/sixth");
 
-        const resultsEl = await driver.findElements(By.tagName("td"));
-        const promisesRes = resultsEl.map(el => el.getText());
-        let resultsAll = await Promise.all(promisesRes);
-        let resultsPage = resultsAll
-            .filter((res, index) => {
-                return (index + 1) % 2 === 0;
-            })
-            .join("");
-        const wordEl = await driver.findElement(By.id("word")).getText();
-        let word = await Promise.resolve(wordEl);
-        word = word.split("");
-        const myResultObj = word.reduce((total, letter) => {
-            if (total[letter]) {
-                total[letter]++;
-            } else {
-                total[letter] = 1;
-            }
-            return total;
-        }, {});
-        let myResults = Object.keys(myResultObj)
-            .map(key => myResultObj[key])
-            .join("");
-        assert(resultsPage === myResults, "Exam #6 ERROR!");
-    });
+    const resultsEl = await driver.findElements(By.tagName("td"));
+    const promisesRes = resultsEl.map(el => el.getText());
+    let resultsAll = await Promise.all(promisesRes);
+    let resultsPage = resultsAll
+      .filter((res, index) => {
+        return (index + 1) % 2 === 0;
+      })
+      .join("");
+    const wordEl = await driver.findElement(By.id("word")).getText();
+    let word = await Promise.resolve(wordEl);
+    word = word.split("");
+    const myResultObj = word.reduce((total, letter) => {
+      if (total[letter]) {
+        total[letter]++;
+      } else {
+        total[letter] = 1;
+      }
+      return total;
+    }, {});
+    let myResults = Object.keys(myResultObj)
+      .map(key => myResultObj[key])
+      .join("");
+    assert(resultsPage === myResults, "Exam #6 ERROR!");
+  });
 
-    it("Exam #7 - Validate factorial calculation", async function() {
-        await driver.get("https://itk-qa-exams.itekako.com/seventh");
-        await driver.wait(until.elementLocated(By.tagName("table")), 10000);
+  it("Exam #7 - Validate factorial calculation", async function() {
+    await driver.get("https://itk-qa-exams.itekako.com/seventh");
+    await driver.wait(until.elementLocated(By.tagName("table")), 10000);
 
-        const elements = await driver.findElements(By.tagName("td"));
-        const promises = elements.map(el => el.getText());
-        const values = await Promise.all(promises);
-        const result = Number(values[1]);
-        let myResult = Number(values[0]);
-        for (let i = myResult - 1; i > 1; i--) {
-            myResult *= i;
-        }
-        assert(myResult === result, "Exam #7 ERROR!");
-    });
+    const elements = await driver.findElements(By.tagName("td"));
+    const promises = elements.map(el => el.getText());
+    const values = await Promise.all(promises);
+    const result = Number(values[1]);
+    let myResult = Number(values[0]);
+    for (let i = myResult - 1; i > 1; i--) {
+      myResult *= i;
+    }
+    assert(myResult === result, "Exam #7 ERROR!");
+  });
 
-    it("Exam #8 - Validate result of fixed number of operands (in this case 5) and subtraction as operation", async function() {
-        await driver.get("https://itk-qa-exams.itekako.com/eighth");
+  it("Exam #8 - Validate result of fixed number of operands (in this case 5) and subtraction as operation", async function() {
+    await driver.get("https://itk-qa-exams.itekako.com/eighth");
 
-        const parent = await driver.findElement(
-            By.className("main flex-center")
-        );
-        const valueDiv = await parent.findElements(By.tagName("div"));
-        const valuesProm = valueDiv.map(div => div.getText());
-        const values = await Promise.all(valuesProm);
+    const parent = await driver.findElement(By.className("main flex-center"));
+    const valueDiv = await parent.findElements(By.tagName("div"));
+    const valuesProm = valueDiv.map(div => div.getText());
+    const values = await Promise.all(valuesProm);
 
-        const result = Number(values.pop());
-        const myResult = values.reduce((total, val, i) => {
-            if (i === 0) {
-                total += val;
-                return total;
-            } else {
-                total -= val;
-                return total;
-            }
-        }, 0);
-        assert(result === myResult, "Exam #8 ERROR!");
-    });
+    const result = Number(values.pop());
+    const myResult = values.reduce((total, val, i) => {
+      if (i === 0) {
+        total += val;
+        return total;
+      } else {
+        total -= val;
+        return total;
+      }
+    }, 0);
+    assert(result === myResult, "Exam #8 ERROR!");
+  });
 
-    it("Exam #9 - Validate result of fixed number of operands (in this case 3) and addition as operation", async function() {
-        await driver.get("https://itk-qa-exams.itekako.com/ninth");
+  it("Exam #9 - Validate result of fixed number of operands (in this case 3) and addition as operation", async function() {
+    await driver.get("https://itk-qa-exams.itekako.com/ninth");
 
-        const elementsVal = await driver.findElements(By.className("value"));
-        const promises = elementsVal.map(el => el.getText());
-        const numbers = await Promise.all(promises);
-        const sum = numbers.reduce((total, num) => (total += Number(num)), 0);
-        const resultDiv = await driver.findElement(By.className("result"));
-        const resultImgEl = await resultDiv.findElements(By.tagName("img"));
-        const promisesImgsrc = resultImgEl.map(el => el.getAttribute("src"));
-        const imgSrc = await Promise.all(promisesImgsrc);
-        let result = imgSrc
-            .map(src => {
-                return src.charAt(src.length - 5);
-            })
-            .join("");
-        result = Number(result);
-        assert(result == sum, "Exam #9 ERROR!");
-    });
-    after(() => driver && driver.quit());
+    const elementsVal = await driver.findElements(By.className("value"));
+    const promises = elementsVal.map(el => el.getText());
+    const numbers = await Promise.all(promises);
+    const sum = numbers.reduce((total, num) => (total += Number(num)), 0);
+    const resultDiv = await driver.findElement(By.className("result"));
+    const resultImgEl = await resultDiv.findElements(By.tagName("img"));
+    const promisesImgsrc = resultImgEl.map(el => el.getAttribute("src"));
+    const imgSrc = await Promise.all(promisesImgsrc);
+    let result = imgSrc
+      .map(src => {
+        return src.charAt(src.length - 5);
+      })
+      .join("");
+    result = Number(result);
+    assert(result == sum, "Exam #9 ERROR!");
+  });
+  after(() => driver && driver.quit());
 });
